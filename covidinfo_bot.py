991c49d9f55a9f0f00c0e540a52f13ecb5803f55
Trinh Tuan Kiet <76782169+TuaKie@users.noreply.github.com>
2021-11-18 19:45:29 +0700

    Add files via upload

--- covidinfo_bot.py
+++ covidinfo_bot.py
@@ -1,22 +1,28 @@
 #import library
 import requests
-from win10toast import ToastNotifier
+#pip install win10toast
+from win10toast import ToastNotifier 
 import json
 import time 
 #Function Update
 def update():
+    #this is world's covid-19 information
     #World
     r=requests.get('https://coronavirus-19-api.herokuapp.com/all')
     data=r.json()
     text=f'Cases: {data["cases"]} \nDeaths: {data["deaths"]} \nRecovered: {data["recovered"]}'
+    #You can change your country by change API https://coronavirus-19-api.herokuapp.com/countries/{yourcountry}
     #VietNam
     r1=requests.get("https://coronavirus-19-api.herokuapp.com/countries/vietnam")
     data=r1.json()
-    text1=f'Country: {data["country"]}\nCases: {data["cases"]} \nToday_Cases: {data["todayCases"]} \nDeaths: {data["deaths"]} \nToday_Deaths: {data["todayDeaths"]} \nRecovered: {data["recovered"]}'
+    #2 type data !! Choose 1 or 2 
+    #text1=f'Cases: {data["cases"]} \n \nDeaths: {data["deaths"]}  \nRecovered: {data["recovered"]}'
+    text1=f'Today_Cases: {data["todayCases"]} \nToday_Deaths: {data["todayDeaths"]} \nRecovered: {data["recovered"]}'
     #Output
     while True:
         t=ToastNotifier()
         t.show_toast("Covid 19 Update ",text, duration=20)
-        t.show_toast("Covid 19 Update ",text1, duration=20)
+        t.show_toast(("Covid 19 Update "+ "-" +  f'{data["country"]}'),text1, duration=20)
+        #300 seconds later, there is an announcement. You can change time if you want
         time.sleep(300)
 update()
